235b8318b33ff87b6b98b0519273cb9a3824faa8
Jean-Lou <jeanlou601@hotmail.com>
2016-09-23 13:29:23 -0400

    Ajout fonction "longueurString"

--- tp1.c
+++ tp1.c
@@ -10,10 +10,20 @@
 #define HAUTEUR_MAX 15
 #define LARGEUR_MAX 20
 
+// ---------- //
+//            //
+//------------//
+
 int main(int argc, char *argv[]){
     if(argc < 4 || argc > 4){
         printf("Nombre d'arguments invalides: il en faut 3\n");
     }else{
         printf("test tp1 okay\n");
     }
+}
+
+int longeurString(char* str){
+    int longueur;
+    longueur = strlen(str);
+    return longueur; 
 }
